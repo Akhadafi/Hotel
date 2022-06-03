30278acc522d3d90fbbf4b8e561a76a6aaf4e216
Akhadafi <khadafiar495@gmail.com>
2022-06-03 20:06:55 +0700

    Edit Halaman Akun Pengguna

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from .models import Message, Room, Topic
 
 
@@ -188,3 +188,17 @@ def deleteMessage(request, pk):
         return redirect("home")
 
     return render(request, "base/delete.html", {"obj": message})
+
+
+@login_required(login_url="login")
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect("user-profile", pk=user.id)
+
+    return render(request, "base/update-user.html", {"form": form})
